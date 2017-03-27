d914d38317d4d175d50ef15cd0253fbea5a2f35b
vicky <lch82327@gmail.com>
2017-03-27 14:54:11 +0800

    change log msg

--- update_ubike_stations.rb
+++ update_ubike_stations.rb
@@ -10,10 +10,10 @@ ENV.update config
 res = HTTP.get('https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.gz')
 
 if res.code != 200
-  puts "Get Data Failed!"
+  puts "Get Stations Data Failed!"
   return
 else
-  puts "Get Data Successlly!"
+  puts "Get Stations Data Successfully!"
 end
 gz = Zlib::GzipReader.new(StringIO.new(res.body.to_s))
 data = JSON.parse(gz.read)
@@ -22,8 +22,8 @@ data = JSON.parse(gz.read)
 body = {"records":data['retVal'].values}
 res = HTTP.basic_auth(:user => ENV['ACCOUNT'], :pass => ENV['PASSWORD'])
           .post("https://nearest-ubike.herokuapp.com/v1/update", :json => body)
-if res.code !=202
-  puts "Update Data Failed!"
+if res.code != 202
+  puts "Update Stations Failed!"
 else
-  puts "Update Data Successlly!"
+  puts "Update Stations Successfully!"
 end
